21635480152a24aa6010e0aff42f86b06aa96182
HoangLinh51 <linhle5112002@gmail.com>
2024-05-22 16:31:19 +0700

    tv-page

--- src/app/modules/home/movie/tv-list/tv-list.component.ts
+++ src/app/modules/home/movie/tv-list/tv-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { PaginatorState } from 'primeng/paginator';
+import { MovieService } from 'src/app/service/movie.service';
 
 @Component({
   selector: 'app-tv-list',
@@ -6,22 +8,38 @@ import { Component, ElementRef, Input, ViewChild } from '@angular/core';
   styleUrls: ['./tv-list.component.scss']
 })
 export class TvListComponent {
-  @ViewChild('listContainer', { static: true }) listContainer!: ElementRef;
-  @Input() listMovie:any
-  @Input() list!: string
-    
+  dataResults: any
+  totalPages: number = 0;
+  currentPage: number = 0;
+  category= 'tv'
+  list: string=''
   imgUrl = 'https://image.tmdb.org/t/p/original';
-  scrollLeft() {
-    this.listContainer.nativeElement.scrollBy({
-      left: -630,
-      behavior: 'smooth',
-    });
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit() {
+    this.getListMovie('tv', 'airing_today', 1)
   }
 
-  scrollRight() {
-    this.listContainer.nativeElement.scrollBy({
-      left: 630,
-      behavior: 'smooth',
-    });
+  getListMovie(category: string, list: string, page: number) {
+    this.movieService.getList(category,list, page).subscribe(
+      (data) => {      
+        console.log('data.results', data)
+        this.dataResults = data.results;
+        this.list = list
+        this.totalPages = data.total_pages
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
+  }
+  
+  onPageChange(event: PaginatorState) {
+    if (event.page !== undefined) {
+      console.log('this.currentPage', this.currentPage)
+      this.currentPage = event.page + 1;
+      console.log('currentPage',this.currentPage )    
+      this.getListMovie(this.category, this.list, this.currentPage);
+    }
   }
 }
